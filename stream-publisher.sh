f1e18c4c8576946fe6012e3d472619bf78df89c9
lin chen <lin.chen@protechna.de>
2026-04-03 11:42:30 +0200

    perf: lower latency in stream-publisher.sh

--- stream-publisher.sh
+++ stream-publisher.sh
@@ -5,5 +5,5 @@ export LIVEKIT_PUBLISH_TOKEN=$(lk token create --room myroom --identity gst-prod
 export ENCODE_TYPE=x264enc
 
 gstreamer-publisher --url "$LIVEKIT_URL" --token "$LIVEKIT_PUBLISH_TOKEN" -- \
-  "v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1,width=1280,height=720 ! jpegdec ! videoconvert ! $ENCODE_TYPE tune=zerolatency key-int-max=60 bitrate=2000" \
+  "v4l2src device=/dev/video0 ! image/jpeg,framerate=30/1,width=1280,height=720 ! jpegdec ! videoconvert ! $ENCODE_TYPE tune=zerolatency speed-preset=ultrafast bframes=0 key-int-max=30" \
   "alsasrc device=hw:0 ! audioconvert ! audioresample ! opusenc bitrate=64000"
